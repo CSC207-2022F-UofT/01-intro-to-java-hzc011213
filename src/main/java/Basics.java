6d978ecca60567350bf1dc8f77a22a0299ad7bd0
Jimmy Hao <hzc011213@gmail.com>
2022-10-09 23:46:55 -0400

    Remove TODO

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -40,9 +40,8 @@ public class Basics {
          */
         System.out.println(7 + 5);
 
-        /* TODO (Task 1): Write a line of code below that prints the string
-         *                Hello World!
-         */
+
+
         System.out.println("Hello World!");
 
 
@@ -60,8 +59,7 @@ public class Basics {
          */
 
         /*
-         * TODO (Task 2): Create a variable named my_variable and assign it the
-         *                value 100.
+
          */
         int my_variable = 100;
 
@@ -90,9 +88,7 @@ public class Basics {
          */
 
         /*
-         * TODO (Task 3): Create a for-loop that loops from 10 to 0, printing
-         *                the following at each iteration:
-         *                Current count: #
+
          *
          * e.g. the end result should look like:
          * Current count: 10
@@ -137,13 +133,8 @@ public class Basics {
      * @return           The first letter of every word in to_split
      */
     public static String split(String to_split) {
-        /* TODO (Task 4): Complete this method body.
-         *                The String methods .split and .charAt may be helpful,
-         *                along with the StringBuilder.append
-         *                You may also assume that to_split always has 7 words
-         *                separated by spaces, so no loops are required, though
-         *                you may use them if you wish.
-         */
+
+
         StringBuilder ret = new StringBuilder();
 
         for (String a : to_split.split(" ")) {
@@ -171,12 +162,8 @@ public class Basics {
     public static int oddSum(int[] arr) {
         int current_sum = 0;
 
-        /* TODO (Task 5): Complete this method body using a for-loop.
-         *                You can find the length of an array by using the
-         *                .length attribute (e.g. arr.length)
-         *                You can index into arrays as we do in Python
-         *                (e.g. arr[i] gives you the item at index i).
-         */
+
+
         for (int i = 0; i < arr.length; i++) {
             if (i % 2 != 0) {
                 current_sum += arr[i];
@@ -188,9 +175,7 @@ public class Basics {
 
 
     /*
-     * TODO (Task 6): Submit the changes you made on GitHub!
-     *                When you submit it, go to the 'Actions' tab. You should
-     *                be able to see this exercise being autograded!
+
      *
      *                You can click on the ongoing action to see more details.
      *                If the tests don't pass, look at the results and revise
